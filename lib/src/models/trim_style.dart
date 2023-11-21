ebc60175ff1f2a8fbdabd442d8e5949d3cc8b074
Alejandro Gómez Rivas <a.ribas@topfollowers.com>
2023-11-21 14:00:11 -0500

    Changed styles

--- lib/src/models/trim_style.dart
+++ lib/src/models/trim_style.dart
@@ -9,7 +9,7 @@ class TrimSliderStyle {
     this.background = Colors.black38,
     this.positionLineColor = Colors.pinkAccent,
     this.positionLineWidth = 2,
-    this.lineColor = Colors.pinkAccent,
+    this.lineColor = Colors.pink,
     this.onTrimmingColor = kDefaultSelectedColor,
     this.onTrimmedColor = kDefaultSelectedColor,
     this.lineWidth = 2,
